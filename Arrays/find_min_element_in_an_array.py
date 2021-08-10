36799f204eb16e37db1e9ceb7a4ce2261ebfa467
fuadsaneen <fuadsaneen@gmail.com>
2021-08-10 23:33:50 +0530

    added find_min()

---
+++ Arrays/find_min_element_in_an_array.py
@@ -0,0 +1,34 @@
+# Program : Find the minimum element in an array.
+# Input : size = 5 and array = [1, 3, 5, 2, 4]
+# Ouput : 1
+# Language : Python3
+
+# O(n) time | O(1) space
+def find_min_element(size, array):
+
+    # Assume that 1st element is the minimum element in the array.
+    min_element = array[0]
+
+    # Do for all elements from position 1 to size-1
+    for i in range(1, size):
+        # Declare the current element.
+        current_element = array[i]
+        # If current_element is less than min_element, then update min_element.
+        if current_element < min_element:
+            min_element = current_element
+
+    # Return the min_element.
+    return min_element 
+
+# Main function.
+if __name__ == '__main__':
+
+    # Declare the size and array.
+    size = 5
+    array = [1, 3, 5, 2, 4]
+
+    # Find minimum element and store it in answer variable.
+    answer = find_min_element(size, array)
+
+    # Print answer.
+    print(answer)
