f65a2db87a83447bd479dc31baddd2e8f1c2902f
fuadsaneen <fuadsaneen@gmail.com>
2021-08-11 10:38:09 +0530

    Updated

---
+++ Numbers/absolute_value.py
@@ -0,0 +1,27 @@
+# Program : Find the absolute value of the given number.
+# Input : number = -3
+# Output : 3
+# Explanation : The absolute value of a number is the distance to that number from origin, so here it is 3.
+# Language : Python3
+
+# O(1) time | O(1) space
+def absolute_value(number):
+
+    # If the number is less than zero, then its absolute value is -(number).
+    if number < 0:
+        return -(number)
+    # Otherwise, ie, if the number is greater than or equal to zero, then its absolute value is the number itself.    
+    else:
+        return number     
+
+# Main function.
+if __name__ == '__main__':
+
+    # Declare the number.
+    number = -3
+
+    # Find the absolute value of the given number and store the result in answer variable.
+    answer = absolute_value(number)
+
+    # Print answer.
+    print(answer)
