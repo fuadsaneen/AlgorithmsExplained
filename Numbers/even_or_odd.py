3328853d4b094df40256b2573b6539e59cde2ac0
fuadsaneen <fuadsaneen@gmail.com>
2021-08-11 09:11:50 +0530

    Updated

--- Numbers/even_or_odd.py
+++ Numbers/even_or_odd.py
@@ -4,10 +4,13 @@
 # Explanation : 5 is not divisible by 2, so it is an odd number.
 # Language : Python3
 
+# O(1) time | O(1) space
 def even_or_odd(number):
     
+    # If the number is divisible by two, then it is even.
     if number % 2 == 0:
         return "Even"
+    # Otherwise, ie, if the number is not divisible by two, then it is odd.    
     else:
         return "Odd"    
 
